9643033603e08e6f0b392a83673ffe6c8ec251f0
Pasha Mukovoz <pashamukovoz@bcd074a14d46.lan>
2024-05-27 14:53:45 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import http.HttpServer;
 
 public class Main {
   public static void main(String[] args) {
-    HttpServer server = new HttpServer(4221);
+    HttpServer server = new HttpServer(4221, 10);
     server.serve();
   }
 }
--- src/main/java/http/HttpServer.java
+++ src/main/java/http/HttpServer.java
@@ -6,15 +6,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import app.Router;
 import app.handlers.RequestHandler;
 
 public class HttpServer {
     final int port;
-    public HttpServer(int port){
-        this.port = port;
-    }
+      private final ExecutorService executorService;
+
+
+  public HttpServer(final int port, final int concurrencyLevel) {
+    this.port = port;
+    this.executorService = Executors.newFixedThreadPool(concurrencyLevel);
+  }
 
     public ServerSocket createServerSocket() throws IOException {
         ServerSocket serverSocket = new ServerSocket(this.port);
@@ -26,26 +32,36 @@ public class HttpServer {
         return serverSocket.accept();
     }
 
+    private void handleRequest(Socket clientSocket){
+        try {
+            System.out.println("accepted new connection");
+    
+            InputStream input = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            Request request = new Request(reader);
+            RequestHandler handler = new Router().getHandler(request);
+    
+            request.print();
+    
+            Response response = handler.handle(request);
+            output.write(response.encode());
+    
+    
+            } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            } 
+    }
+
     public void serve() {
     try {
 
         ServerSocket serverSocket = this.createServerSocket();
-        Socket clientSocket = this.getClientScoket(serverSocket); // Wait for connection from client.
-       
-        System.out.println("accepted new connection");
-
-        InputStream input = clientSocket.getInputStream();
-        OutputStream output = clientSocket.getOutputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        Request request = new Request(reader);
-        RequestHandler handler = new Router().getHandler(request);
-
-        request.print();
-
-        Response response = handler.handle(request);
-        output.write(response.encode());
-
-
+        serverSocket.setReuseAddress(true);
+        while (true) { 
+            Socket clientSocket = this.getClientScoket(serverSocket); // Wait for connection from client.
+            this.executorService.submit(() -> this.handleRequest(clientSocket));
+        }
         } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
         } 
